dfb636d9456dfb273027723e3bc9fdffb5218e53
evgeniy-bond <evgeniy.bondarev.01@gmail.com>
2025-03-21 19:56:24 +0700

    feat: APP-955 added bascule check to sdk

---
+++ packages/sdk/src/web3Sdk/getBasculeDepositStatus/utils/const.ts
@@ -0,0 +1,7 @@
+import { OChainId } from '../../../common/types/types';
+
+export const BASCULE_SUPPORTED_CHAINS: number[] = [
+  OChainId.ethereum,
+  OChainId.base,
+  OChainId.binanceSmartChain,
+];
