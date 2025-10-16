4062aff1dbfcba96d06ff26f72d1019c3c0bf1d0
Meagan Autrey <106602832+megatron757@users.noreply.github.com>
2025-10-16 10:00:12 -0500

    update bft-dev url (#1037)
    Co-authored-by: Mikias Abera <itsmikias@gmail.com>

--- packages/sdk/src/common/api-config.ts
+++ packages/sdk/src/common/api-config.ts
@@ -21,7 +21,7 @@ const prodConfig: IApiConfig = {
 };
 
 const devConfig: IApiConfig = {
-  baseApiUrl: 'https://bft-dev.stage.lombard.finance',
+  baseApiUrl: 'https://bft-dev.stage.lombard-fi.com',
   // Note, bff on localhost works on 8001
   bffApiUrl: 'https://bff.stage.lombard-fi.com',
 };
