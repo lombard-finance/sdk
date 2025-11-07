dcb9a832441bd1dfefbbfc4a9d58a1e8f6a921a6
evgenii-bond <evgenii@lombard.finance>
2025-11-07 22:57:36 +0700

    chore: update gastald endpoints (#1214)

--- packages/sdk/src/common/api-config.ts
+++ packages/sdk/src/common/api-config.ts
@@ -11,7 +11,7 @@ const stageConfig: IApiConfig = {
 };
 
 const testnetConfig: IApiConfig = {
-  baseApiUrl: 'https://gastald-testnet.prod.lombard.finance',
+  baseApiUrl: 'https://gastald-testnet.prod.lombard-fi.com',
   bffApiUrl: 'https://bff.stage.lombard-fi.com',
 };
 
@@ -21,7 +21,7 @@ const prodConfig: IApiConfig = {
 };
 
 const devConfig: IApiConfig = {
-  baseApiUrl: 'https://bft-dev.stage.lombard.finance',
+  baseApiUrl: 'https://bft-dev.stage.lombard-fi.com',
   // Note, bff on localhost works on 8001
   bffApiUrl: 'https://bff.stage.lombard-fi.com',
 };
