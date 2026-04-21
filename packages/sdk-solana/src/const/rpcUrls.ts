08ca1dcac6a986c2185a7ac915f62359130ab672
evgenii-bond <evgenii@lombard.finance>
2026-04-21 22:35:48 +0700

    feat: chore

--- packages/sdk-solana/src/const/rpcUrls.ts
+++ packages/sdk-solana/src/const/rpcUrls.ts
@@ -14,8 +14,7 @@ export const BFF_WS_URL_STAGE =
 export const RPC_URLS: Record<SolanaNetwork, string> = {
   [SolanaNetwork.mainnet]: `${BFF_BASE_URL_PROD}/solana`,
   [SolanaNetwork.testnet]: 'https://api.testnet.solana.com',
-  // [SolanaNetwork.devnet]: `${BFF_BASE_URL_PROD}/solana_devnet`,
-  [SolanaNetwork.devnet]: 'https://api.devnet.solana.com',
+  [SolanaNetwork.devnet]: `${BFF_BASE_URL_PROD}/solana_devnet`,
 };
 
 export const WS_URLS: Record<SolanaNetwork, string> = {
